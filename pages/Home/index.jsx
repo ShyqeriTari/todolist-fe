50b6a41200b2ea34ab63b391725f7a7b64fae69a
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-08-07 16:32:16 +0200

    delete todo

--- pages/Home/index.jsx
+++ pages/Home/index.jsx
@@ -9,15 +9,20 @@ const Home = () => {
   const [todos, setTodos] = useState(undefined);
   const [show, setShow] = useState(false);
   const [show2, setShow2] = useState(false);
+  const [show3, setShow3] = useState(false);
   const [todoView, setTodoView] = useState(undefined);
   const [title, setTitle] = useState(undefined);
   const [content, setContent] = useState(undefined);
+  const [deleteId, setDeleteId] = useState(undefined);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
     const handleClose2 = () => setShow2(false);
     const handleShow2 = () => setShow2(true);
+
+    const handleClose3 = () => setShow3(false);
+    const handleShow3 = () => setShow3(true);
 
   const router = useRouter();
 
@@ -36,7 +41,6 @@ const Home = () => {
           }
       });
       const data = await response.json();
-      console.log(data)
       setTodos(data);
       
     } catch (error) {
@@ -59,7 +63,7 @@ const Home = () => {
         if (response.ok) {
             let data = await response.json()
             console.log(data)
-          setShow2(false)
+         handleClose2()
           getTodos()
         } else {
             alert("Creating todo failed")
@@ -75,6 +79,31 @@ const Home = () => {
     }
 }
 
+const deleteTodo = async () => {
+  try {
+    let response = await fetch(`${process.env.NEXT_PUBLIC_LOCAL_URL}/todo/` + deleteId, {
+    method: "DELETE",
+    headers: {
+      "Authorization": `Bearer ${localStorage.getItem("token")}`
+    },
+    })
+    if (response.status === 200) {
+      handleClose3()
+      getTodos()
+    } else {
+    alert("dletion failed")
+    if (response.status === 400) {
+      alert("bad request")
+    }
+    if (response.status === 404) {
+      alert("page not found")
+    }
+    }
+  } catch (error) {
+    console.log(error)
+  }
+  }
+
   useEffect(() => {getTodos()} ,[])
 
   return (
@@ -87,14 +116,17 @@ const Home = () => {
     <h1 className="text-center font-bold text-blue-600 mb-5">TODOS</h1>
     <div className="grid gap-4 sm:grid-cols-2 m-auto md:grid-cols-4 lg:grid-cols-6">
       {todos && todos.map((todo, index) => { return(
-        <div key={index} onClick={()=> {handleShow(); setTodoView(todo)}} className="note-container pointer  p-2 m-auto flex flex-col justify-between">
-        <div>
+        <div key={index}  className="note-container pointer  p-2 m-auto flex flex-col justify-between">
+        <div onClick={()=> {handleShow(); setTodoView(todo)}}>
           <h5 className="font-bold text-blue-600 mb-2">{todo.title}</h5>
           <div>
           <span className="text-sm cut-line">{todo.content}</span>
           </div> 
         </div>
-        <span className="block mt-auto font-light text-xs">{todo.updatedAt.slice(0,10) + " · " + todo.updatedAt.slice(11,19)}</span>
+        <div className="flex justify-between items-center">
+        <span onClick={()=> {handleShow(); setTodoView(todo)}} className="mt-auto font-light text-xs">{todo.updatedAt.slice(0,10) + " · " + todo.updatedAt.slice(11,19)}</span>
+        <i className="bi bi-trash pointer text-red-600" onClick={()=>{handleShow3(); setDeleteId(todo.id)}} style={{fontSize: "20px"}}></i>
+        </div>
       </div>)
       })}
     </div>
@@ -130,6 +162,26 @@ const Home = () => {
         <Modal.Body><input type="text" placeholder="Insert title..." onChange={(e) => { setTitle(e.target.value) }} required className="w-70 m-auto mt-3 mb-3 border-blue-600 border p-1" />
         <textarea required placeholder="Insert content..." onChange={(e)=> {setContent(e.target.value)}} className="border p-1 border-blue-600" name="Text1" cols="40" rows="5"></textarea>
         <button className="mt-3 mb-2 m-auto border-container bg-blue-600 text-white" onClick={(e) => { createTodo(e) }} >Submit</button></Modal.Body>
+      </Modal>
+
+      <Modal
+        size="md"
+        show={show3}
+        onHide={() => handleClose3()}
+        aria-labelledby="example-modal-sizes-title-md"
+      >
+        <Modal.Header >
+          <Modal.Title id="example-modal-sizes-title-md" className="text-blue-600">
+           Delete todo
+          </Modal.Title>
+          <i className="bi bi-x-circle-fill pointer text-blue-600" onClick={handleClose3} style={{fontSize: "25px"}}></i>
+        </Modal.Header>
+        <Modal.Body><h6>Are you sure to delete todo?</h6>
+        <div className="flex">
+          <button className="mt-3 mb-2 m-auto border-container bg-green-600 text-white" onClick={(e) => { deleteTodo() }} >Yes</button> 
+        <button className="mt-3 mb-2 m-auto border-container bg-red-600 text-white" onClick={(e) => { handleClose3() }} >no</button>
+        </div>
+        </Modal.Body>
       </Modal>
 </>
   )
