78521d9574cf67ad8d425b16a1ea7664a6d7ee29
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-08-07 13:10:32 +0200

    roter to home component and logout

--- pages/Home/index.jsx
+++ pages/Home/index.jsx
@@ -1,8 +1,21 @@
+import { useRouter } from "next/router";
 
-const Index = () => {
+const Home = () => {
+
+    const router = useRouter();
+
+    const Logout = () => {
+        localStorage.removeItem("token")
+        localStorage.removeItem("userId")
+        router.push('/')
+      }
+
  return( <div>
+    <div className="flex justify-end">
+<div className="border-container2 text-center p-2 mt-2 mr-2 pointer" onClick={()=> {Logout()}} >Logout</div>
+</div>
 <h1>Home</h1>
   </div>)
 };
 
-export default Index;
+export default Home;
