0d8e1676d72e692d5dc938a22ad3a3bf20c9c04e
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-08-07 16:46:44 +0200

    modify todo

--- pages/Home/index.jsx
+++ pages/Home/index.jsx
@@ -10,10 +10,12 @@ const Home = () => {
   const [show, setShow] = useState(false);
   const [show2, setShow2] = useState(false);
   const [show3, setShow3] = useState(false);
+  const [show4, setShow4] = useState(false);
   const [todoView, setTodoView] = useState(undefined);
   const [title, setTitle] = useState(undefined);
   const [content, setContent] = useState(undefined);
   const [deleteId, setDeleteId] = useState(undefined);
+  const [modifyId, setModifyId] = useState(undefined);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -23,6 +25,9 @@ const Home = () => {
 
     const handleClose3 = () => setShow3(false);
     const handleShow3 = () => setShow3(true);
+
+    const handleClose4 = () => setShow4(false);
+    const handleShow4 = () => setShow4(true);
 
   const router = useRouter();
 
@@ -104,6 +109,35 @@ const deleteTodo = async () => {
   }
   }
 
+  const modifyTodo = async (e) => {
+    e.preventDefault();
+    const Todo = { title, content }
+    try {
+        let response = await fetch(`${process.env.NEXT_PUBLIC_LOCAL_URL}/todo/` + modifyId, {
+            method: "PUT",
+            body: JSON.stringify(Todo),
+            headers: {
+                "Content-type": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem("token")}`
+            },
+        })
+        if (response.status === 204) {
+         handleClose4()
+          getTodos()
+        } else {
+            alert("Creating todo failed")
+            if (response.status === 400) {
+                alert("bad request")
+            }
+            if (response.status === 404) {
+                alert("page not found")
+            }
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
   useEffect(() => {getTodos()} ,[])
 
   return (
@@ -125,7 +159,8 @@ const deleteTodo = async () => {
         </div>
         <div className="flex justify-between items-center">
         <span onClick={()=> {handleShow(); setTodoView(todo)}} className="mt-auto font-light text-xs">{todo.updatedAt.slice(0,10) + " · " + todo.updatedAt.slice(11,19)}</span>
-        <i className="bi bi-trash pointer text-red-600" onClick={()=>{handleShow3(); setDeleteId(todo.id)}} style={{fontSize: "20px"}}></i>
+        <i className="bi bi-pencil pointer text-green-600" onClick={()=>{handleShow4(); setModifyId(todo.id); setTodoView(todo)}} style={{fontSize: "18px"}}></i>
+        <i className="bi bi-trash pointer text-red-600" onClick={()=>{handleShow3(); setDeleteId(todo.id)}} style={{fontSize: "18px"}}></i>
         </div>
       </div>)
       })}
@@ -182,6 +217,23 @@ const deleteTodo = async () => {
         <button className="mt-3 mb-2 m-auto border-container bg-red-600 text-white" onClick={(e) => { handleClose3() }} >no</button>
         </div>
         </Modal.Body>
+      </Modal>
+
+      <Modal
+        size="md"
+        show={show4}
+        onHide={() => handleClose4()}
+        aria-labelledby="example-modal-sizes-title-md"
+      >
+        <Modal.Header >
+          <Modal.Title id="example-modal-sizes-title-md" className="text-blue-600">
+           Modify todo
+          </Modal.Title>
+          <i className="bi bi-x-circle-fill pointer text-blue-600" onClick={handleClose4} style={{fontSize: "25px"}}></i>
+        </Modal.Header>
+        <Modal.Body><input type="text" defaultValue={todoView && todoView.title} onChange={(e) => { setTitle(e.target.value) }} required className="w-70 m-auto mt-3 mb-3 border-blue-600 border p-1" />
+        <textarea required defaultValue={todoView && todoView.content} onChange={(e)=> {setContent(e.target.value)}} className="border p-1 border-blue-600" name="Text1" cols="40" rows="10"></textarea>
+        <button className="mt-3 mb-2 m-auto border-container bg-blue-600 text-white" onClick={(e) => { modifyTodo(e) }} >Submit</button></Modal.Body>
       </Modal>
 </>
   )
